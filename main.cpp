45550284fd72a6a40e6faab3e42d30b56ef24009
Jayhyuk Choi <jay@inha.edu>
2021-09-19 20:52:55 +0900

    NEW foo fucntion!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
+void foo(){
+    cout << "hello form JAY branch";
+}
 int main(){
     cout << "hello main";
-    cout << "hello form JAY branch";
+    foo();
+
+    return 0;
 }
