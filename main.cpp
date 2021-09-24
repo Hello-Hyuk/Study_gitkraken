5b85b0364f393b2644eb596b946a3d3eb51d5a11
Jayhyuk Choi <jay@inha.edu>
2021-09-24 18:03:32 +0900

    ww

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-<<<<<<<<< Temporary merge branch 1
 void user2(){
     cout << "1212121 User2" << endl;
 }
@@ -12,16 +11,5 @@ int main(){
     user2();
     user1();
 
-=======
-void User3(){
-    cout << "user3" << endl;
-}
-int main(){
-    user2();
-    user1();
-
-    cout << "hello main";
-    User3();
->>>>>>>>> Temporary merge branch 2
     return 0;
 }
