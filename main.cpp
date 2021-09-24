77159459ba7af238436e76a43a55bbc0e3707c3f
Jayhyuk Choi <jay@inha.edu>
2021-09-24 17:40:49 +0900

    init main

--- main.cpp
+++ main.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
 using namespace std;
 
-void foo(){
-    cout << "hello from JAY branch";
-}
-void foo2(){
-    cout << "hello from jay2";
-}
-void foo3(){
-    cout << "hello from Choi";
-}
-void foo4(){
-    cout << "hello from jay2 and 4";
-}
 int main(){
     cout << "hello main";
-    foo();
-    foo2();
-    foo3();
-    foo4();
 
     return 0;
 }
