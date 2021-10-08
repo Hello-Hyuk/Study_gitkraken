6cd51b075ff5cdf0a886ffa2fdc70e0452bb9d83
Jayhyuk Choi <jay@inha.edu>
2021-10-09 06:34:02 +0900

    switch

--- cv.py
+++ cv.py
@@ -1,7 +1,7 @@
 class JAY:
     def __init__(self) -> None:
-        self.num = 12171863
         self.name = "jay"
+        self.num = 12171863
     def printINFO(self):
         print(self.num,self.name)
 class USER1:
