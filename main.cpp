11f2df81b72fa570cbc69c8d2b9ce65d681ed795
Jayhyuk Choi <jay@inha.edu>
2021-10-09 06:43:50 +0900

    user3

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-void func2(){
+void func3(){
     cout << "hello from func2" << endl;
 }
 
 int main(){
     cout << "hello from main" << endl;
-    func2();
+    func3();
     return 0;
 }
