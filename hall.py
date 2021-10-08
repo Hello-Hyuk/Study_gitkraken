2af1896b4fab904b8d61423e5189f83580755cd8
Jayhyuk Choi <jay@inha.edu>
2021-10-09 04:48:02 +0900

    hal

---
+++ hall.py
@@ -0,0 +1,6 @@
+class HAL:
+    def __init__(self) -> None:
+        self.num = 12171863
+        self.name = "jay"
+    def printINFO(self):
+        print(self.num,self.name)
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import cv
-
+import hall
 jay = cv.JAY()
 user = cv.USER1()
+hal = hall.HAL
 print("show cv member info")
 jay.printINFO()
 user.printINFO()
+hal.printINFO()
 
 
