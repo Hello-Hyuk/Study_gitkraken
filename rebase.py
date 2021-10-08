dd1e468264c78cffd3ff5232876828f5a6592a8b
Jayhyuk Choi <jay@inha.edu>
2021-10-09 04:07:14 +0900

    class rebase

--- rebase.py
+++ rebase.py
@@ -1,7 +1,7 @@
-class JAY:
+class REBASE:
     def __init__(self) -> None:
-        self.num = 12171863
-        self.name = "jay"
+        self.num = 1212121
+        self.name = "rebase"
     def printINFO(self):
         print(self.num,self.name)
         
