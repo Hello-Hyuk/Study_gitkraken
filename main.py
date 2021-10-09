aebe43cc34a49f3505e5c6a46d52991acf8993d7
ka-yeon <12181736@inha.edu>
2021-10-09 16:52:59 +0900

    kayeon hello
    hello

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import vision as vs
 vision = vs.VISION()
 vision.printINFO()
 
-
+kayeon = vs.KAYEON()
+kayeon.printINFO()
--- vision.py
+++ vision.py
@@ -5,5 +5,10 @@ class VISION:
         print(self.init)
 
 
-            
-        
+class KAYEON:
+    def __inint__(self) -> None:
+        self.name = "Kayeon"
+        self.team = "team3"
+        self.num = 12181736
+    def printINFO(self):
+        print(self.name, self.team, self.num)
