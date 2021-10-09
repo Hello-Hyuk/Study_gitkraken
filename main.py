17fbc0a653c98ae4f0e65913fcdc8ac89696244f
Jayhyuk Choi <jay@inha.edu>
2021-10-09 13:47:32 +0900

    2

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import vision as vs
 
 vision = vs.VISION()
+choi = vs.CHOI()
 vision.printINFO()
+choi.printINFO()
 
 
 
--- vision.py
+++ vision.py
@@ -3,6 +3,12 @@ class VISION:
         self.init = "Team vision infomation"
     def printINFO(self):
         print(self.init)
-
+class CHOI:
+    def __init__(self) -> None:
+        self.name = "Team vision infomation"
+        self.team = "team 1"
+        self.num = 12121212
+    def printINFO(self):
+        print(self.name,self.team,self.num)
             
         
