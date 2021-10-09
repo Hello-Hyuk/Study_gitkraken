0fc065f668b7dd9f1c3610d5e8b61aebe64ec23f
ka-yeon <12181736@inha.edu>
2021-10-09 16:18:08 +0900

    hello kayeon
    kayeon

--- vision.py
+++ vision.py
@@ -4,6 +4,12 @@ class VISION:
     def printINFO(self):
         print(self.init)
 
-
+class kayeon:
+    def __init__(self) -> None:
+        self.name = "Kayeon"
+        self.team = "team 3"
+        self.age = "23"
+    def printINFO(selt):
+        print(self.name, self.team, self.age)
             
         
