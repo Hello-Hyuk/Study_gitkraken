47bf8737680e53adbfbc0d57ce27dafc0b4e76e6
최재혁 <74503770+Hello-Hyuk@users.noreply.github.com>
2021-10-09 13:17:37 +0900

    pull rquest (#11)

--- vision.py
+++ vision.py
@@ -3,6 +3,12 @@ class VISION:
         self.init = "Team vision infomation"
     def printINFO(self):
         print(self.init)
-        
+class choi:
+    def __init__(self) -> None:
+        self.name = "choi"
+        self.team = "team 2"
+        self.number = 12171863
+    def printINFO(self):
+        print(self.name, self.team, self.number);
             
         
