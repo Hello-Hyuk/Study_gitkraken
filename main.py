7898a590068abef2681a28afa4ede6d13565710d
seoyoung <yoon3435@inha.edu>
2021-10-09 17:38:53 +0900

    sy

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import cv
-import hall
-jay = cv.JAY()
-user2 = cv.USER2()
-hal = hall.HAL
-print("show cv member info")
-jay.printINFO()
-user2.printINFO()
-hal.printINFO()
+import vision as vs
 
+vision = vs.VISION()
+seoyoung = vs.Seoyoung()
+vision.printINFO()
+seoyoung=vs.Seoyoung()
 
---
+++ vision.py
@@ -0,0 +1,15 @@
+class VISION:
+    def __init__(self) -> None:
+        self.init = "Team vision infomation"
+    def printINFO(self):
+        print(self.init)
+
+class Seoyoung:
+    def __init__(self) -> None:
+        self.name = "seoyoung"
+        self.team = "team 1"
+        self.num = 12191792
+    def printINFO(self):
+        print(self.name, self.team, self.num)
+            
+        
