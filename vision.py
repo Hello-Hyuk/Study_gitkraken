ce0f11aa80ac85e242cb585d8cf6d6c04ef1144b
Doody_joo <wnalsqja@naver.com>
2021-10-09 16:23:56 +0900

    change vision.py

--- vision.py
+++ vision.py
@@ -3,3 +3,9 @@ class VISION:
         self.init = "Team vision infomation"
     def printINFO(self):
         print(self.init)
+
+class MINBUM:
+    def __init__(self) -> None:
+        self.init = "minbum 12151627"
+    def printINFO(self):
+        print(self.init)
